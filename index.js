dfd48e6c9de23e43d589968669d25f68307a2b6a
zannatreya <zannatulbintabahar@yahoo.com>
2022-04-29 00:05:28 +0600

    mongodb user & pass created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -9,6 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 // db name:warehouseManagement collection:product
+
+
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6kj22.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/', (req, res) => {
     res.send('running warehouse management server');
