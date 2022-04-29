5ee09da965859d4a9435ba67028b6253d09d76b5
zannatreya <zannatulbintabahar@yahoo.com>
2022-04-29 22:29:49 +0600

    updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6kj22.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
+const warehousecollection = client.db("warehouseManagement").collection("product");
 
 app.get('/', (req, res) => {
     res.send('running warehouse management server');
