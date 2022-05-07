d882f8ff49d9a2c2eb6d1843d31f14d9af5f46ed
zannatreya <zannatulbintabahar@yahoo.com>
2022-05-07 18:01:42 +0600

    item add & delete implemented

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -12,10 +12,46 @@ app.use(express.json());
 // db name:warehouseManagement collection:product
 
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6kj22.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6kj22.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const warehousecollection = client.db("warehouseManagement").collection("product");
+async function run() {
+    try {
+        await client.connect();
+        const warehousecollection = client.db("warehouseManagement").collection("product");
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = warehousecollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await warehousecollection.findOne(query);
+            res.send(product);
+        });
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await warehousecollection.insertOne(newProduct);
+            res.send(result);
+        });
+
+        // DELETE
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await warehousecollection.deleteOne(query);
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('running warehouse management server');
